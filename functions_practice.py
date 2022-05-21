716ad2fee2d609583b72b2bae5007ceea3a63e5d
Zecival <Zecivalx@gmail.com>
2022-05-21 09:43:23 -0400

    first commit

---
+++ functions_practice.py
@@ -0,0 +1,19 @@
+def hello():
+    print("hello")
+
+def pack(a,b,c):
+    return(a,b,c)
+
+def eat_lunch(my_list):
+    if len(my_list) > 0:
+        print(f'First I eat {my_list[0]}')
+        for index, item in enumerate(my_list):
+            if index > 0:
+                print(f'Next, I eat {item}')
+    else:
+        print('My lunchbox is empty')
+
+hello()
+my_list = pack('Ham', 'Bread', 'Juice')
+eat_lunch(my_list)
+
